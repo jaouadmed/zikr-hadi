5dc037ef4f79a9cc82ef3520a65b185608005841
jaouadmed <jaouadmohamed0@gmail.com>
2021-04-01 02:16:17 +0200

    color wl3ibaat

--- src/main/webapp/app/entities/type/type.module.ts
+++ src/main/webapp/app/entities/type/type.module.ts
@@ -6,7 +6,7 @@ import { TypeDetailComponent } from './detail/type-detail.component';
 import { TypeUpdateComponent } from './update/type-update.component';
 import { TypeDeleteDialogComponent } from './delete/type-delete-dialog.component';
 import { TypeRoutingModule } from './route/type-routing.module';
-import { ColorPickerModule } from 'primeng/colorpicker';
+import { ColorPickerModule } from 'ngx-color-picker';
 
 @NgModule({
   imports: [SharedModule, TypeRoutingModule, ColorPickerModule],
