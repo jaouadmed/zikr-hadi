aaf57dc9d9a6924b168d4063ae0ceff9737ef677
jaouadmed <jaouadmohamed0@gmail.com>
2021-04-01 12:15:25 +0200

    final touches

--- src/main/webapp/app/entities/type/type.module.ts
+++ src/main/webapp/app/entities/type/type.module.ts
@@ -6,9 +6,10 @@ import { TypeDetailComponent } from './detail/type-detail.component';
 import { TypeUpdateComponent } from './update/type-update.component';
 import { TypeDeleteDialogComponent } from './delete/type-delete-dialog.component';
 import { TypeRoutingModule } from './route/type-routing.module';
+import { ColorPickerModule } from 'ngx-color-picker';
 
 @NgModule({
-  imports: [SharedModule, TypeRoutingModule],
+  imports: [SharedModule, TypeRoutingModule, ColorPickerModule],
   declarations: [TypeComponent, TypeDetailComponent, TypeUpdateComponent, TypeDeleteDialogComponent],
   entryComponents: [TypeDeleteDialogComponent],
 })
